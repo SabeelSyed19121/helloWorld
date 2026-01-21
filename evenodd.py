80e17ee3a251e7f7c89c4a7a59debda674514142
syed sabeel <sabeelsyed19121@gmail.com>
2026-01-21 09:47:29 +0530

    added two files

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+
+# check_even_odd.py
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
+
---
+++ numbers.py
@@ -0,0 +1,6 @@
+
+# print_numbers.py
+
+for i in range(1, 11):
+    print(i)
+
